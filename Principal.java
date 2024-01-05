a5f24897205cb8d67802771c364c4031f1c295f2
Adriano <adrianovendramini@hotmail.com>
2024-01-04 21:48:42 -0300

    projeto alura

---
+++ Principal.java
@@ -0,0 +1,24 @@
+package aluraPO;
+
+import aluraPO.model.Filme;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Filme meuFilme = new Filme();
+		meuFilme.setNome("O podereso chefao");
+		meuFilme.setAnoDeLancamento(1970);
+		meuFilme.setDuracaoEmMinutos(180);
+		
+		
+		meuFilme.exibeFichaTecnica();
+		meuFilme.avalia(8);
+		meuFilme.avalia(5);
+		meuFilme.avalia(10);
+		System.out.println(meuFilme.getTotalDeAvaliacoes());
+//		System.out.println(meuFilme.totalDeAvaliacoes);
+		System.out.println(meuFilme.pegaMedia());
+
+	}
+
+}
